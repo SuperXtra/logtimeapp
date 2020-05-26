c65f571faedf5d7f197fc80fe0989dc3bdbe0f1b
Pawel <pawel.dabala@gmail.com>
2020-05-26 17:12:30 +0200

    untracked files

---
+++ src/main/scala/service/refactor/repo/project/InsertProject.scala
@@ -0,0 +1,20 @@
+package service.refactor.repo.project
+
+import cats.effect.Sync
+import data.Queries
+import doobie.implicits._
+import doobie.postgres.sqlstate
+import doobie.util.transactor.Transactor
+import error.{AppError, CannotLogNewTaskWithDuplicateTaskDescriptionUnderTheSameProject, CannotLogNewTaskWithTheOverlappingTimeRangeForTheSameUser}
+
+
+class InsertProject[F[+_] : Sync](tx: Transactor[F]) {
+  def apply(projectName: String, userId: Long): F[Either[AppError, Long]] =
+    Queries
+      .Project
+      .insert(projectName, userId).transact(tx)
+      .attemptSomeSqlState {
+        case sqlstate.class23.EXCLUSION_VIOLATION => CannotLogNewTaskWithTheOverlappingTimeRangeForTheSameUser
+        case sqlstate.class23.UNIQUE_VIOLATION => CannotLogNewTaskWithDuplicateTaskDescriptionUnderTheSameProject
+      }
+}
---
+++ src/main/scala/service/refactor/serv/project/UpdateProject.scala
@@ -0,0 +1,40 @@
+package service.refactor.serv.project
+
+import cats.data.EitherT
+import cats.effect.Sync
+import data.ChangeProjectName
+import data.Entities.Project
+import doobie.implicits._
+import doobie.postgres.sqlstate
+import doobie.util.transactor.Transactor
+import error.{AppError, CannotChangeNameGivenProjectNameExistsAlready, ProjectNotCreated, UserNotFound}
+import service.refactor.repo.user.GetExistingUserId
+import service.refactor.repo.project.{FindProjectById, UpdateProjectName}
+
+
+class UpdateProject[F[+_]: Sync](tx: Transactor[F])(
+                                userId: GetExistingUserId[F],
+                                updateProjectName: UpdateProjectName[F],
+                                findProject: FindProjectById[F]) {
+
+
+  def apply(project: ChangeProjectName): F[Either[AppError, Project]] = (for {
+    userId <- getExistingUserId(project.userIdentification)
+    _ <- changeProjectName(project.oldProjectName, project.projectName, userId)
+    updatedRecord <- findProjectById(project.projectName)
+  } yield updatedRecord).value
+
+
+  private def getExistingUserId(userIdentification: String): EitherT[F, AppError, Long] = {
+    EitherT.fromOptionF(userId(userIdentification), UserNotFound)
+  }
+
+  private def findProjectById(projectName: String): EitherT[F, AppError, Project] =
+    EitherT.fromOptionF(findProject(projectName), ProjectNotCreated)
+
+  private def changeProjectName(oldProjectName: String, projectName: String, userId: Long): EitherT[F, AppError, Int] =
+    EitherT(updateProjectName(oldProjectName, projectName, userId).attemptSomeSqlState {
+      case sqlstate.class23.UNIQUE_VIOLATION => CannotChangeNameGivenProjectNameExistsAlready
+    })
+
+}
